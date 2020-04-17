dd582eac7b4bb77c17a0c7b87e1ddaacafeea5ed
Nina Kiwatrowski <nkiwatrowski@web.de>
2020-04-17 14:08:36 +0200

    Update main.cpp

--- src/soft/soft_2dskeletonization/main.cpp
+++ src/soft/soft_2dskeletonization/main.cpp
@@ -54,7 +54,7 @@ using namespace std;
 using namespace cv;
 
 //Declaration default values
-string inputImgDefault = "rat-01.png";
+string inputImgDefault = "oben.png";
 string skeletonImgNameDefault = "skeleton.png";
 string filenameEnding = "-Epsilon1px-skeleton.png";
 double epsilonValueDefault = 50.0;

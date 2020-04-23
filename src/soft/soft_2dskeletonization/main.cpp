89528ed806526729511142c49be8bc93492a3222
Nina Kiwatrowski <nkiwatrowski@web.de>
2020-04-23 13:55:28 +0200

    Update main.cpp

--- src/soft/soft_2dskeletonization/main.cpp
+++ src/soft/soft_2dskeletonization/main.cpp
@@ -54,7 +54,7 @@ using namespace std;
 using namespace cv;
 //TODO delete
 //Declaration default values
-string inputImgDefault = "RK5_20200104_SHSY5Y_R_2500_01_Alexa488_02.png";
+string inputImgDefault = "RK5_20200104_SHSY5Y_R_2500_02_Alexa488_01.png";
 string skeletonImgNameDefault = "skeleton.png";
 string filenameEnding = "-Epsilon1px-skeleton.png";
 double epsilonValueDefault = 10.0;

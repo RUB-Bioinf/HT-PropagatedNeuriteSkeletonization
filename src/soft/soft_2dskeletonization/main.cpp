51062df7657613120c8b1a113d720a9df41792da
Nina K <nkiwatrowski@web.de>
2020-07-03 15:37:51 +0200

    Update main.cpp

--- src/soft/soft_2dskeletonization/main.cpp
+++ src/soft/soft_2dskeletonization/main.cpp
@@ -204,7 +204,7 @@ int main(int argc, char **argv) {
     auto str = oss.str();
     prefix = str;
     system("../test.sh");
-    system(" /opt/fiji/Fiji.app/ImageJ-linux64 -ij2 --headless --console -macro ../test2.ijm ../ressources/");
+    system(" /opt/fiji/Fiji.app/ImageJ-linux64 -ij2 --headless --console -macro ../makroWithoutBrightnessChanging.ijm ../ressources/");
     resultFilename = "../output/"+ prefix + "/resultData.csv";
     inputValuesRead(argc, argv);
 

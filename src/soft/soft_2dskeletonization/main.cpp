662b58e5ab6647aef577fb5a503bf1fbe40482cb
Nils Fo <nils.foerster-m9n@rub.de>
2021-02-01 17:31:22 +0100

    Tests to make the pipeline runnable

--- src/soft/soft_2dskeletonization/main.cpp
+++ src/soft/soft_2dskeletonization/main.cpp
@@ -216,7 +216,7 @@ int main(int argc, char **argv) {
     auto str = oss.str();
     prefix = str;
     system("../test.sh");
-    system(" /opt/fiji/Fiji.app/ImageJ-linux64 -ij2 --headless --console -macro ../frida.ijm ../resources/");
+    system(" /bph/puredata4/bioinfdata/work/HT-PropagatedNeuriteSkeletonization/S-Modul-Data/fiji/Fiji.app/ImageJ-linux64 -ij2 --headless --console -macro ../frida.ijm ../resources/");
     resultFilename = "../output/"+ prefix + "/resultData.csv";
     inputValuesRead(argc, argv);
 

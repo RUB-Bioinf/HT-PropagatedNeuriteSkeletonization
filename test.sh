d0a65c41a61e68be137a45282b9d789f32a37e79
RUB Bioinformatics Group <nils.foerster@bph.rub.de>
2021-02-02 19:44:45 +0100

    Update test.sh

--- test.sh
+++ test.sh
@@ -1,3 +1,6 @@
+echo Running test-sh
+chmod +x ../resources
+chmod +x ../output
 cd ../resources
 find . -type d -print0 >../dirs.txt
 cd ../output
@@ -7,3 +10,4 @@ mkdir -p "$(date +%R)"
 cd "$(date +%R)"
 xargs -0 mkdir -p <../../../dirs.txt
 rm ../../../dirs.txt
+echo Finished Running test-sh

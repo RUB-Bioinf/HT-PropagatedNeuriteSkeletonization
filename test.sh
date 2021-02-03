00d67867459a495df62e7ce3e06d35e78f6567e5
Nils Fo <nils.foerster-m9n@rub.de>
2021-02-03 23:00:57 +0100

    Updated test.sh to maybe eliminate permissions conflicts?

--- test.sh
+++ test.sh
@@ -4,13 +4,13 @@ whoami
 chmod +x ../resources
 chmod +x ../output
 cd ../resources
-find . -type d -print0 >../dirs.txt
+find . -type d -print0 > /out-tree/dirs.txt
 cd ../output
 mkdir -p "$(date +"%d-%m-%Y")"
 cd "$(date +"%d-%m-%Y")"
 mkdir -p "$(date "+%H-%M")"
 cd "$(date "+%H-%M")"
-xargs -0 mkdir -p <../../../dirs.txt
+xargs -0 mkdir -p < /out-tree/dirs.txt
 rm ../../../dirs.txt
 echo finished running test-sh!
 sleep 5

4d4d172bb34d81e938c39b7793f07c06a343efcc
Nils Fo <nils.foerster-m9n@rub.de>
2021-02-03 22:21:34 +0100

    Updating test.sh for windows compatibility

--- test.sh
+++ test.sh
@@ -8,8 +8,8 @@ find . -type d -print0 >../dirs.txt
 cd ../output
 mkdir -p "$(date +"%d-%m-%Y")"
 cd "$(date +"%d-%m-%Y")"
-mkdir -p "$(date +%R)"
-cd "$(date +%R)"
+mkdir -p "$(date "+%H-%M")"
+cd "$(date "+%H-%M")"
 xargs -0 mkdir -p <../../../dirs.txt
 rm ../../../dirs.txt
 echo finished running test-sh!

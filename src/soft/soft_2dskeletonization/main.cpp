d9e878414a73ea3c2caec07bad0d89d6871e03a8
Lahutar <33023364+Lahutar@users.noreply.github.com>
2021-06-10 13:53:02 +0200

    Update main.cpp

--- src/soft/soft_2dskeletonization/main.cpp
+++ src/soft/soft_2dskeletonization/main.cpp
@@ -718,7 +718,7 @@ void getOptBnd(skeleton::GraphSkel2d::Ptr grskelpropag, const boundary::Discrete
     ofstream csvFile(csvFilename);
     for(unsigned int i = 0; i < optiBnd.size(); i++) 
     {
-        auto g = optiBnd[w];
+        auto g = optiBnd[i];
         csvFile <<  g.coords[0] << "," << g.coords[1] << "; \n";
     }
   //for (auto a : optiBnd){  
